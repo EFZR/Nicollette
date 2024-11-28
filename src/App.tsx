7b45774a7039ed64a954071c5df6a777c7222320
efzr <emerson.zapatarivas@gmail.com>
2024-11-28 03:41:39 +0000

    Little change to start working on the header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
+import Header from "./components/Header";
+
 function App() {
-  return (
-      <p>Vite + React</p>
-  )
+  return <Header />;
 }
 
-export default App
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+import { BiX, BiMenu } from "react-icons/bi";
+import "@styles/Header.css";
+
+export default function Header() {
+  return <div>Test</div>;
+}
