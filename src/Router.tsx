e682117a7f1e67f6f11a46f743f74f30ab285757
efzrbhc <ezapata@banhcafe.hn>
2024-12-31 06:22:08 +0000

    Feat: Hero component initialized

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "@components/Layout";
+import Home from "./pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    children: [],
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
   },
 ]);
 
